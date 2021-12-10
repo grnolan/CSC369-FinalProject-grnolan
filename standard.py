483cbbaaac20427665abb7f67cbf6e5451a6805c
grnolan <49251194+grnolan@users.noreply.github.com>
2021-12-10 10:42:35 -0800

    Update standard.py

--- standard.py
+++ standard.py
@@ -1,22 +1,36 @@
 # Standard Approach 
 
 import json
+import time
 
 def main():
+    
+    print("--- STANDARD LANGUAGES ---")
     # language: number of repos 
-    f = open('LANNGS1000rows.json')
+    f = open('langs.json')
     data = json.load(f)
+    start_time = time.time()
     count_languages(data)
+    print("--- %s seconds ---" % (time.time() - start_time))
     
+
+    print("--- STANDARD FILES ---")
     # repo_name: file count and average # files per repo
-    f = open('FILES1000rows.json')
+    f = open('files.json')
     data = json.load(f)
+    start_time = time.time()
     get_file_stats(data)
+    print("--- %s seconds ---" % (time.time() - start_time))
     
+    
+    print("--- STANDARD COMMITS ---")
     # repo_name: commit count and average # commits per repo
-    f = open('COMMITS100.json')
+    f = open('commits.json')
     data = json.load(f)
+    start_time = time.time()
     get_commit_stats(data)
+    print("--- %s seconds ---" % (time.time() - start_time))
+    
         
 def count_languages(data):
     langs = {}
@@ -31,10 +45,22 @@ def count_languages(data):
                     langs[row['language'][i]['name']] += 1
                 else:
                     langs[row['language'][i]['name']] = 1
-            
+                    
+    #print all results     
+    lesses = 0
+    tot = 0
     for l in langs.keys():
-        print(l + " : " + str(langs[l]))
-
+        tot += langs[l]
+        if langs[l] > 1000:
+            print(l + ":" + str(langs[l]))
+        else:
+            lesses += 1
+    
+    most = max(langs, key=langs.get)
+    print(most)
+    print(langs[most])
+    
+    
         
 def get_file_stats(data):
     repos = {}
@@ -45,8 +71,9 @@ def get_file_stats(data):
         else:
             repos[row['repo_name']] = 1
     
-    for r in repos.keys():
-        print(r + " : " + str(repos[r]))
+    #print all results
+    #for r in repos.keys():
+    #    print(r + " : " + str(repos[r]))
         
     #find average
     total = 0
@@ -63,19 +90,22 @@ def get_commit_stats(data):
     repos = {}
     
     for row in data:
-        for repo in row['repo_name']:
-            if repo in repos.keys():
-                repos[repo] += 1
-            else:
-                repos[repo] = 1
+        repo = row['repo_name']
+        if repo in repos.keys():
+            repos[repo] += 1
+        else:
+            repos[repo] = 1
     
     for r in repos.keys():
         print(r + " : " + str(repos[r]))
-        
+
+    #find average
     total = 0
     n = 0
     avg = 0
     for r in repos.keys():
+        print(r)
+        print(repos[r])
         total += repos[r]
         n += 1
     avg = total / n
