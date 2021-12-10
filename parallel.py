712a72fd624a60243dc4957acef9ce66e0122f92
grnolan <49251194+grnolan@users.noreply.github.com>
2021-12-10 10:37:38 -0800

    Update parallel.py

--- parallel.py
+++ parallel.py
@@ -1,5 +1,6 @@
 
 import json
+import time
 
 import numpy as np
 
@@ -17,37 +18,42 @@ def main():
     spark = SparkSession(sc)
     
     # language: number of repos 
-    f = open('LANNGS1000rows.json')
-    data = json.load(f)    
-    dataRDD = sc.parallelize(data)  
-    count_languages(spark, dataRDD)
+    print("--- PARALLEL LANGUAGES ---")
+    df = spark.read.option("multiline", "true").json('langs.json')
+    start_time = time.time()
+    count_languages(spark, df)
+    print("--- %s seconds ---" % (time.time() - start_time))
     
     
     # repo_name: file count and average # files per repo
-    df = spark.read.option("multiline", "true").json('FILES1000rows.json')
+    print("--- PARALLEL FILES ---")
+    df = spark.read.option("multiline", "true").json('files.json')
+    start_time = time.time()
     get_file_stats(spark, df)
+    print("--- %s seconds ---" % (time.time() - start_time))
 
     
     # repo_name: commit count and average # commits per repo
-    df = spark.read.option("multiline", "true").json('COMMITS1000.json')
+    print("--- PARALLEL COMMITS ---")
+    df = spark.read.option("multiline", "true").json('commits.json')
+    start_time = time.time()
     get_commit_stats(spark, df)
+    print("--- %s seconds ---" % (time.time() - start_time))
 
     
-def count_languages(spark, dataRDD):
-    # dataframe
-    data = spark.read.json(dataRDD)
-    df = data.select(data.repo_name, explode(data.language))
+def count_languages(spark, df):
+
+    df = df.select("repo_name", explode("language"))
     df = df.na.drop()
-    df = df.select(df.col)
-    #df.printSchema()
-    #df.show()
+    df = df.select("col")
     
     #RDD
-    rdd = df.rdd.map(lambda row: (row.col.name, 1))
+    rdd = df.rdd.map(lambda row: (row.col[1], 1))
     counts = rdd.reduceByKey(lambda a,b: a + b)
-    output = counts.collect()
-    for o in output:
-        print(o[0] + ": " + str(o[1]))
+#    output = counts.collect()
+#    for o in output:
+#        print(o[0] + ": " + str(o[1]))
+    print(counts.max(lambda x:x[1]))
                
         
 def get_file_stats(spark, df):
@@ -55,23 +61,25 @@ def get_file_stats(spark, df):
     #RDD
     rdd = df.rdd.map(lambda row: (row.repo_name, 1))
     counts = rdd.reduceByKey(lambda a,b: a + b)
-    output = counts.collect()
-    for o in output:
-        print(o)
+#    output = counts.collect()
+#    for o in output:
+#        print(o)
         
     #find average
     avg = counts.map(lambda row: row[1]).mean()
     print("There are " + str(avg) + " files on average in a repository")
             
-    
+
+        
 def get_commit_stats(spark, df):
-    df.show()
-    df = df.select(df.commit, df.repo_name)
-    rdd = df.rdd.map(lambda row: (row.repo_name[0], 1))    
+
+    rdd = df.rdd.map(lambda row: (row.repo_name, 1)) 
     counts = rdd.reduceByKey(lambda a,b: a + b)
-    output = counts.collect()
-    for o in output:
-        print(o)
+    
+    # PRINT RESULTS
+#    output = counts.collect()
+#    for o in output:
+#        print(o)
         
     #find average
     avg = counts.map(lambda row: row[1]).mean()
